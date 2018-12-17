e96bac6345eefab7622b5584ee0239783a2d3542
vlastik <vlastikczech1@gmail.com>
2018-12-16 20:21:47 -0700

    Added a component to allow new questions to be submitted to the backend

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import {Route} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import Questions from './Questions/Questions';
 import Question from './Question/Question';
-
 import Callback from './Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
             <Route exact path='/' component={Questions}/>
             <Route exact path='/question/:questionId' component={Question}/>
             <Route exact path='/callback' component={Callback}/>
+            <SecuredRoute path='/new-question' component={NewQuestion}/>
         </div>
 
     );
